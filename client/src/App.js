5934a3f62f58d948dae3fb2354a3be02b776d3a4
Ventsislav57 <106383945+Ventsislav57@users.noreply.github.com>
2022-08-07 11:39:57 +0300

    add broserRouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
-// import Login from './components/Auth/Login';
-// import Register from './components/Auth/Register';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
 // import BurgerMenu from './components/Menu/BurgerMenu';
 // import Menu from './components/Menu/Menu';
 
@@ -13,8 +13,8 @@ function App() {
 
             <Routes>
                 <Route path='/' element={<Home />} />
-                {/* <Route path='/login' element={<Login />} /> */}
-                {/* <Route path='/register' element={<Register />} /> */}
+                <Route path='/login' element={<Login />} />
+                <Route path='/register' element={<Register />} />
                 {/* <Route path='/menu' element={<Menu />} /> */}
                 {/* <Route path='/burger-menu' element={<BurgerMenu />} /> */}
 
