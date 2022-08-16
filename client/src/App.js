b8217d361b2ede916d0004f2ea387724e2f225bb
Ventsislav57 <106383945+Ventsislav57@users.noreply.github.com>
2022-08-16 13:02:43 +0300

    add private route

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-
+import { Suspense } from 'react';
 import { AuthProvider } from './context/AuthContext';
 
 import Navbar from "./components/Header/Header";
@@ -13,6 +13,7 @@ import BurgerDetails from './components/Menu/Burger/BurgerDetails';
 import PizzaMenu from './components/Menu/Pizza/PizzaMenu';
 import Reservation from './components/Reservation/Reservation';
 import PrivateRoute from './components/common/PrivateRoute';
+import Profile from './components/Profile/Profile-information/Profile';
 
 function App() {
 
@@ -27,16 +28,29 @@ function App() {
                     <Route path='/' element={<Home />} />
                     <Route path='/login' element={<Login />} />
                     <Route path='/register' element={<Register />} />
-                    <Route path='/logout' element={<Logout />} />
+                    <Route path='/logout' element={
+                        <PrivateRoute>
+                            <Logout />
+                        </PrivateRoute>
+                    } />
                     <Route path='/menu' element={<Menu />} />
                     <Route path='/reservation' element={
                         <PrivateRoute>
                             <Reservation />
                         </PrivateRoute>
                     } />
-                    <Route path='/burger-menu' element={<BurgerMenu />} />
+                    <Route path='/burger-menu' element={
+                        <Suspense fallback={<span>Loading....</span>}>
+                            <BurgerMenu />
+                        </Suspense>
+                    } />
                     <Route path='/burger/details/:burgerId' element={<BurgerDetails />} />
                     <Route path='/pizza-menu' element={<PizzaMenu />} />
+                    <Route path='/profile' element={
+                        <PrivateRoute>
+                            <Profile />
+                        </PrivateRoute>
+                    } />
 
                 </Routes>
 
